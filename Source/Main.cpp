cb6021a9b5c39a35b0463c964936d2f1017c9343
Imran1720 <skimran1720@gmail.com>
2025-02-12 15:26:54 +0530

    Added EventManager and window close on escape button pressed.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+//#include <SFML/Graphics.hpp>
 #include "../Header/Core/GameWindowManager.h"
+#include "../Header/Event/EventManager.h"
+
 int main()
 {
 	//Game window manager instance
     Core::GameWindowManager gameWindowManager;
 
+	Events::EventManager eventManager;
 	//window initialization
     gameWindowManager.initialize();
 
 
 	while (gameWindowManager.isGameRunning())
 	{
+		eventManager.pollEvents(gameWindowManager.getGameWindow());
 		gameWindowManager.render();
 	}
 
